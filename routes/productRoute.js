51cf4d8c5716ff251aed9306cfcddb1f8ae8eccb
Puneeth-Kumar-B <101204256+Puneeth-Kumar-B@users.noreply.github.com>
2022-07-25 17:52:14 +0530

    Update productRoute.js

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -2,7 +2,16 @@ const express = require("express");
 const { Validations } = require("../validations/productValidator");
 const router = express.Router();
 const Controller = require('../controllers/productController')
+const upload = require('../middleware/upload')
 
+router.post("/upload", function(req, res) {
+    upload.single('uploadImages')(req, res, function(error) {
+        if (error) {
+            return res.json({ error: "Error occured while uploading image" })
+        }
+        res.status(200).json({ message: "Image Uploaded Successfully", ImageUrl: `http://localhost:8000/${req.file.path}` })
+    })
+})
 
 router.post("/add", Validations, Controller.addProduct)
 router.get('/search', Controller.searchProduct)
